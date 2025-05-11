2ae0192e5560520e1498dff53e2f4ebda8d4f63d
Ifaa Store <114418374+liaacans@users.noreply.github.com>
2025-05-11 17:11:40 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,104 @@
+
+
+
+
+
+
+#! bin/sh
+
+
+clear
+echo "tunggu"
+sleep 5
+clear
+echo "sebentar!!"
+sleep 3
+clear
+echo "wait... sedang menginstall project package termux!"
+sleep 2
+clear
+
+
+
+
+# install package!
+pkg update -y
+clear
+
+pkg upgrade -y
+clear
+
+pkg install git -y
+clear
+
+pkg install ffmpeg -y
+clear
+
+pkg install python -y
+clear
+
+pkg install python2 -y
+clear
+
+pkg install mc -y
+clear
+
+pkg install nodejs -y
+clear
+
+pkg upgrade nodejs -y
+clear
+
+pip install mechanize
+clear
+
+pip2 install mechanize
+clear
+
+pkg install lolcat -y
+clear
+
+pip2 install lolcat
+clear
+
+pkg install figlet -y
+clear
+
+pkg install libxml2 -y
+clear
+
+pkg install libxslt -y
+clear
+
+pip install requests
+clear
+
+pip install bs4
+clear
+
+pip2 install requests
+clear
+
+pip2 install bs4
+clear
+
+echo "success, menginstall project package termux!"
+
+
+clear
+
+
+echo "(⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠)"
+sleep 3
+clear
+echo "✧⁠\⁠(⁠>⁠o⁠<⁠)⁠ﾉ⁠✧"
+sleep 2
+clear
+echo "(⁠っ⁠.⁠❛⁠ ⁠ᴗ⁠ ⁠❛⁠.⁠)⁠っ"
+sleep 1
+clear
+echo "Thanks To :
+Allah swt
+Nabi Muhammad
+Aulia Rahman
+And Pengguna Menginstal Package Termux!"
